14a6a48edc758fb2303f047547b41b75aa4360b6
hfaran <hamzafaran@hotmail.com>
2014-06-07 17:58:01 -0700

    Add invalidation flag

--- timetabler/ssc_conn.py
+++ timetabler/ssc_conn.py
@@ -23,20 +23,22 @@ class SSCConnection(object):
         if not os.path.exists(self.cache_path):
             os.mkdir(self.cache_path)
 
-    def get_course_page(self, dept="CPSC", course="304", sessyr="2014", sesscd="W"):
+    def get_course_page(self, dept="CPSC", course="304", sessyr="2014", sesscd="W", invalidate=False):
         """Get course page from SSC
 
         Retrieved pages are cached for a ``self.cache_period``, and then invalidated.
 
-        :type dept: str
-        :type course: str|int
-        :type sessyr: str|int
-        :type sesscd: str
+        :type  dept: str
+        :type  course: str|int
+        :type  sessyr: str|int
+        :type  sesscd: str
+        :type  invalidate: bool
+        :param invalidate: If this is set, existing cache for the page will be invalidated
         :returns: Text of SSC course page for given course
         """
         page_name = "_".join(map(lambda x: str(x).lower(), [dept, course, sessyr, sesscd]))
         # Attempt to retrieve already cached page
-        page = self._retrieve_cached_page(page_name)
+        page = self._retrieve_cached_page(page_name, invalidate=invalidate)
         # If not already cached, retrieve, cache, and return
         if page is None:
             if DEBUG: print("Page was not found in cache or was invalidated; retrieving from remote and caching...")
@@ -61,16 +63,17 @@ class SSCConnection(object):
         with open(filename, 'w+') as f:
             f.write(text)
 
-    def _retrieve_cached_page(self, name):
+    def _retrieve_cached_page(self, name, invalidate=False):
         filename = os.path.join(self.cache_path, name)
         # First case, cache does not already exist
         if not os.path.exists(filename):
             return None
-        # Check if existing cache is invalid (if so, remove and return None)
+        # Check if existing cache is stale
         last_modified = os.path.getmtime(filename)
         period = time.time() - last_modified
         if DEBUG: print("Page was last fetched {:.0f} seconds ago.".format(period))
-        if period > self.cache_period:
+        # If cache is stale, or an invalidation was requested, remove
+        if any([period > self.cache_period, invalidate]):
             os.remove(filename)
             return None
         # Cache exists, and is valid, so return it

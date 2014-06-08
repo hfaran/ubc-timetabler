60fe6f85b0327159a1f732f66f9a80b39077cbd6
hfaran <hamzafaran@hotmail.com>
2014-06-08 15:29:40 -0700

    Add missing constraint for GEOG 122

--- example.py
+++ example.py
@@ -1,14 +1,20 @@
 from datetime import datetime
 
 from timetabler.schedule import Schedule
+from timetabler.ssc.course import Lecture, Discussion
 
 
 def main():
     s = Schedule(["EECE 353", "CPSC 304", "EECE 381", "GEOG 122"],
                  session="2014W", terms=[2])
+    # STTs are for Vantage College students
     s.courses["GEOG 122"].add_constraint(
         lambda acts: all(a.status not in [u"STT"] for a in acts)
     )
+    # Default sections Tutorial (for Vantage)
+    s.courses["GEOG 122"].num_section_constraints = [
+        (Lecture, 1), (Discussion, 1)
+    ]
     scheds = s.generate_schedules()
 
 

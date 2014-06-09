8d111ce1d313fc2a2acafd68840a7c206c75bb69
hfaran <hamzafaran@hotmail.com>
2014-06-08 23:49:44 -0700

    Add latest daily morning; remove other useless sort

--- example.py
+++ example.py
@@ -6,17 +6,20 @@ from timetabler import sort
 
 
 def main():
-    s = Scheduler(["EECE 353", "CPSC 304", "EECE 381", "GEOG 122", "CPSC 317"],
-                  session="2014W", terms=[2], refresh=False)
-    # STTs are for Vantage College students
-    s.courses["GEOG 122"].add_constraint(
-        lambda acts: all(a.status not in [u"STT"] for a in acts)
-    )
-    # Default sections contained a Tutorial but that is for Vantage
-    # students, so removing that and only setting Lecture and Discussion
-    s.courses["GEOG 122"].num_section_constraints = [
-        (Lecture, 1), (Discussion, 1)
-    ]
+    courses = ["EECE 353", "CPSC 304", "EECE 381", "GEOG 122", "CPSC 420"]
+    s = Scheduler(courses, session="2014W", terms=[2], refresh=False)
+
+    if "GEOG 122" in courses:
+        # STTs are for Vantage College students
+        s.courses["GEOG 122"].add_constraint(
+            lambda acts: all(a.status not in [u"STT"] for a in acts)
+        )
+        # Default sections contained a Tutorial but that is for Vantage
+        # students, so removing that and only setting Lecture and Discussion
+        s.courses["GEOG 122"].num_section_constraints = [
+            (Lecture, 1), (Discussion, 1)
+        ]
+
     return s.generate_schedules()
 
 
@@ -29,7 +32,9 @@ if __name__ == '__main__':
         datetime.now() - start_time
     ))
     # Sort and draw
+    scheds = sort.sum_latest_daily_morning(scheds)
     scheds = sort.least_time_at_school(scheds)
+
     for sched in scheds:
         sched.draw(term=2)
         raw_input("Press ENTER to display the next schedule...")
--- timetabler/sort.py
+++ timetabler/sort.py
@@ -2,23 +2,30 @@
 
 from timetabler.util import DAY_LIST, strtime2num
 
-def sum_latest_start_times(schedules):
-    """Sort so that the sum of starting times for courses throughout the week are greatest"""
-    key = lambda s: sum(int(a.start_time.replace(":", "")) for a in s.activities)
-    return sorted(
-        schedules,
-        key=key,
-        reverse=True
-    )
+
+def sum_latest_daily_morning(schedules):
+    """Sort for the latest daily morning"""
+
+    def key(s):
+        total = 0
+        for day in DAY_LIST:
+            acts = s.activities_for_day(day)
+            earliest_start_time = strtime2num(min(a.start_time for a in acts))
+            total += earliest_start_time
+        return total
+
+    return sorted(schedules, key=key, reverse=True)
+
 
 def least_time_at_school(schedules):
     def key(s):
         total = 0
         for day in DAY_LIST:
             acts = s.activities_for_day(day)
-            earliest_start_time =  strtime2num(min(a.start_time for a in acts))
+            earliest_start_time = strtime2num(min(a.start_time for a in acts))
             latest_end_time = strtime2num(max(a.end_time for a in acts))
             time_at_school = latest_end_time - earliest_start_time
             total += time_at_school
         return total
+
     return sorted(schedules, key=key)

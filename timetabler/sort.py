a6f5446761921eb0b6320548fb4ec9a7b14fdb81
hfaran <hamzafaran@hotmail.com>
2014-06-09 22:52:38 -0700

    Add earliest_start and latest_end helpers

--- timetabler/sort.py
+++ timetabler/sort.py
@@ -10,7 +10,7 @@ def sum_latest_daily_morning(schedules):
         total = 0
         for day in DAY_LIST:
             acts = s.activities_for_day(day)
-            earliest_start_time = strtime2num(min(a.start_time for a in acts))
+            earliest_start_time = earliest_start(acts)
             total += earliest_start_time
         return total
 
@@ -22,8 +22,8 @@ def least_time_at_school(schedules):
         total = 0
         for day in DAY_LIST:
             acts = s.activities_for_day(day)
-            earliest_start_time = strtime2num(min(a.start_time for a in acts))
-            latest_end_time = strtime2num(max(a.end_time for a in acts))
+            earliest_start_time = earliest_start(acts)
+            latest_end_time = latest_end(acts)
             time_at_school = latest_end_time - earliest_start_time
             total += time_at_school
         return total
@@ -37,14 +37,27 @@ def even_time_per_day(schedules):
     As a result, sorting by this gives the most "even" schedule
         in regards to time spent at school every day of the week
     """
+
     def key(s):
         time_at_school_week = []
         for day in DAY_LIST:
             acts = s.activities_for_day(day)
-            earliest_start_time = strtime2num(min(a.start_time for a in acts))
-            latest_end_time = strtime2num(max(a.end_time for a in acts))
+            earliest_start_time = earliest_start(acts)
+            latest_end_time = latest_end(acts)
             time_at_school = latest_end_time - earliest_start_time
             time_at_school_week.append(time_at_school)
         return stddev(time_at_school_week)
 
     return sorted(schedules, key=key)
+
+
+###########
+# Helpers #
+###########
+
+def earliest_start(activities):
+    return strtime2num(min(a.start_time for a in activities))
+
+
+def latest_end(activities):
+    return strtime2num(max(a.end_time for a in activities))

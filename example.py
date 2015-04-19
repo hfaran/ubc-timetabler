d815b23f56ea95a4272fe715ce1ff71e9e4db919
hfaran <hamzafaran@outlook.com>
2015-04-19 00:52:29 -0700

    Add new courses

--- example.py
+++ example.py
@@ -8,23 +8,29 @@ from timetabler.sort import earliest_start  # Helper function (should probably b
 
 
 COMMUTE_HOURS = 1.75
+SESSION = "2015W"
+TERMS = (1, 2)
 
 
 def main():
-    required = ("EECE 353", "CPSC 304", "EECE 381")
+    required = (
+        "CPEN 321",
+        "CPEN 421",
+        "CPEN 422",
+        # "EECE 419",
+        "CPEN 481",
+        "APSC 450"
+    )
     opt = [
-        "CPSC 420",
-        # "GEOG 122",  # Taking CIVL200 in T1, so don't need this anymore
-        "EECE 450",
-        "CPSC 322",
-        "CPSC 317",
-        # "EECE 411",  # If this is selected, it and CPSC317 can be the only, b/c 317 is a co-req
+        "CPEN 442",
+        "CPSC 312"
     ]
-    combs = combinations(opt, r=1)
+    num_required_from_opt = 2
+    combs = combinations(opt, r=num_required_from_opt)
 
     schedules = []
     for courses in [required + comb for comb in combs]:
-        s = Scheduler(courses, session="2014W", terms=[2], refresh=True)
+        s = Scheduler(courses, session=SESSION, terms=TERMS, refresh=False)
         # I don't want any classes that start before 9:00AM
         s.add_constraint(lambda sched: earliest_start(sched.activities) >= 9)
         # Add GEOG122 constraints if we need to
@@ -54,10 +60,11 @@ if __name__ == '__main__':
         datetime.now() - start_time
     ))
     # Sort and draw
+    scheds = sort.free_days(scheds)
     scheds = sort.even_time_per_day(scheds, commute_hrs=COMMUTE_HOURS)
     scheds = sort.sum_latest_daily_morning(scheds)
     scheds = sort.least_time_at_school(scheds, commute_hrs=COMMUTE_HOURS)
-    scheds = sort.free_days(scheds)
     for sched in scheds:
-        sched.draw(term=2, draw_location="browser")
+        for term in TERMS:
+            sched.draw(term=term, draw_location="terminal")
         raw_input("Press ENTER to display the next schedule...")
